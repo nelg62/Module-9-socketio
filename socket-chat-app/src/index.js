3655a7a46f1f277d8968afdfca524da481a86319
nelg62 <glen.harding.nz@gmail.com>
2024-06-27 20:48:05 +1200

    up to adding extras to socket io excersise 5

--- socket-chat-app/src/index.js
+++ socket-chat-app/src/index.js
@@ -9,7 +9,23 @@ app.get("/", (req, res) => {
 });
 io.on("connection", (socket) => {
   io.emit("connection", "a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+  });
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
 server.listen(3000, () => {
   console.log("listening on *:3000");
 });
